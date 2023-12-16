3379319c37f83b65774cf641c0baf0c52a8960f3
tanzilrayhan <tanzilrayhan169@gmail.com>
2023-12-16 22:59:36 +0600

    faq updated

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,10 +1,10 @@
 const Faq = () => {
   return (
-    <div className="max-w-7xl mx-5 py-10 lg:mx-auto">
+    <div className="max-w-7xl mx-5 py-10  lg:mx-auto">
       <h1 className="text-center my-10  text-5xl font-bold">
         Frequently Asked Question (FAQ)
       </h1>
-      <div className="collapse collapse-arrow my-3 bg-sky-200">
+      <div className="collapse collapse-arrow my-3 text-black themeColor">
         <input type="radio" name="my-accordion-2" checked="checked" />
         <div className="collapse-title text-xl font-medium">
         1. How does NexTalent ensure the quality of job opportunities listed on the platform?
@@ -13,7 +13,7 @@ const Faq = () => {
           <p>At NexTalent, we prioritize the quality of job opportunities to provide a positive experience for both job seekers and employers. Our team employs a rigorous vetting process to review and verify job listings, ensuring they align with industry standards and ethical practices. We actively monitor user feedback and engage with our community to continuously enhance the quality assurance measures, fostering a trustworthy and reliable job marketplace.</p>
         </div>
       </div>
-      <div className="collapse collapse-arrow  my-3 bg-sky-200">
+      <div className="collapse collapse-arrow  my-3 text-black themeColor">
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title text-xl font-medium">
         2. What sets NexTalent apart from other online job platforms?
@@ -22,7 +22,7 @@ const Faq = () => {
           <p>NexTalent stands out through its commitment to fostering meaningful connections between talent and employers. Our platform goes beyond traditional job-matching by incorporating innovative features, such as personalized skill assessments, detailed candidate profiles, and interactive employer branding. We prioritize user experience, offering a seamless interface and leveraging cutting-edge technology to create a dynamic and user-centric job marketplace that adapts to the evolving needs of both job seekers and employers.</p>
         </div>
       </div>
-      <div className="collapse collapse-arrow  my-3 bg-sky-200">
+      <div className="collapse collapse-arrow  my-3 text-black themeColor">
         <input type="radio" name="my-accordion-2" />
         <div className="collapse-title text-xl font-medium">
         3. How does NexTalent prioritize diversity and inclusion in its job marketplace?
